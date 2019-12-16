319440929a66c6fe7f0f187b7a8d31de47f81cec
Yogi-Kaklottar <kaklottary@gmail.com>
2019-12-16 13:56:14 +0530

    constant binding

---
+++ src/main/java/com/main/java/ConstantBindingModule.java
@@ -0,0 +1,42 @@
+package com.main.java;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
+
+public class ConstantBindingModule {
+	
+	public static void main(String[] args)
+	{
+		Injector injector=Guice.createInjector(new UrlModule());
+		ConncetionSet con=injector.getInstance(ConncetionSet.class);
+		con.printUrl();
+	}
+
+}
+class ConncetionSet{
+	private String url;
+	
+	@Inject
+	public void getUrlString(@Named("uri")String s)
+	{
+		url=s;
+	}
+	public void printUrl()
+	{
+		System.out.println("URL :"+url);
+	}
+}
+
+class UrlModule extends AbstractModule{
+	
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+			bind(String.class).annotatedWith(Names.named("uri")).toInstance("http://localhost/test");
+	}
+	
+}
